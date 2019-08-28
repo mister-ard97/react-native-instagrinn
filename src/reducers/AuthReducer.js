327a97ae931e4cf6f785efc048419271a23a7670
mrezardi <rezardiansyah1997@gmail.com>
2019-08-28 10:02:02 +0700

    'auth

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,34 @@
+import {
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER,
+    LOGIN_USER_FAIL
+} from '../actions/types';
+
+const INITIAL_STATE = {
+    user: null,
+    loading: null, 
+    checkedAuth: null
+}
+
+export default (state = INITIAL_STATE, action) => {
+    switch(action.type) {
+        case LOGIN_USER_SUCCESS:
+            return {
+                ...INITIAL_STATE,
+                user: action.payload, 
+                checkedAuth: true
+            }
+        case LOGIN_USER:
+            return {
+                ...INITIAL_STATE,
+                checkedAuth: true
+            }
+        case LOGIN_USER_FAIL:
+            return {
+                ...INITIAL_STATE,
+                checkedAuth: true
+            }
+        default:
+            return state;
+    }
+}
